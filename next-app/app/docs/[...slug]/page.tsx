924294fcd192f7ec12662e9b55feb5c9d26fe795
Daiwik Korat <daiwikorat@gmail.com>
2026-01-19 13:09:55 +0530

    Catch all Segments

---
+++ next-app/app/docs/[...slug]/page.tsx
@@ -0,0 +1,25 @@
+export default async function DocSlug({
+    params,
+}: {
+    params: Promise<{ slug: string[] }>;
+}) {
+    const { slug } = await params;
+    
+    if(slug?.length === 2) {
+        return (
+            <>
+                <h1> Slug 1: {slug[0]} Slug 2: {slug[1]}</h1>
+            </>
+        )
+    }
+
+    else if (slug?.length === 1) {
+        return <h1> Viewing docs for feature {slug[0]} </h1>
+    }
+
+    return (
+        <>
+            <h1> slug page. No Slugs Found :( </h1>
+        </>
+    )
+}
